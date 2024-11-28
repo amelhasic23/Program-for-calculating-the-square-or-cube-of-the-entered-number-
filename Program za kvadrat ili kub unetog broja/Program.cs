bbfe99e8b1c643653019fbe6d11245ddec93e4dc
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 09:12:05 +0100

    Commit

---
+++ Program za kvadrat ili kub unetog broja/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Unesite broj n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n % 2 != 0)
+        {
+            Console.WriteLine($"Kvadrat broja ({n}) je {n * n}");
+        }
+        else
+        {
+            Console.WriteLine($"Kub broja ({n}) je {n * n * n}");
+        }
+    }
+}
